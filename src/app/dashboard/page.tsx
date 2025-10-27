b129fb86241612fd2a4a7808decdfbb54e1f93e0
Carlos Altán <117488661+carlosaltan18@users.noreply.github.com>
2025-10-26 19:10:18 -0600

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import DashboardPage from "@/components/pages/DashboardPage";
 export default function Dashboard() {
   return (
     <div>
-      <p>hola</p>
       <DashboardPage />
     </div>
   );
